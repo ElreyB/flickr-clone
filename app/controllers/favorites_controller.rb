15dff4953c243543cbf4fe33215e5cdd5061b170
Elrey Belmonti and Kristen Kulha <elrey+kristen@>
2017-11-29 11:08:06 -0800

    Add crud to favorites controller.

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,34 @@
+class FavoritesController < ApplicationController
+
+  def new
+    @image = Image.find(params[:id])
+    @user = current_user
+    @favorite = Favorite.new
+  end
+
+  def create
+    @image = Image.find(params[:id])
+    @user = current_user
+    @favorite = @user.favorites.new(favorite_params)
+    if @favorite.save
+      flash[:notice] = "Image has been added to your favorites!"
+      redirect_to image_path(@image)
+    else
+      flash[:alert] = "Oops! There are errors!s"
+      render :new
+    end
+  end
+
+  def destroy
+    @favorite = Favorite.find(:image_id)
+    @favorite.destroy
+    @user = current_user
+    flash[:notice] = "Favorite has been removed."
+    redirect_to user_path(@user)
+  end
+
+private
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :image_id)
+  end
+end

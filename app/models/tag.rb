2746ef2920c5b6d76b5f1a1cb856f6cb8d4bdc9f
Elrey Belmonti and Kristen Kulha <elrey+kristen@>
2017-11-29 16:41:50 -0800

    test tags validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :images
+
+  validates :name, uniqueness: true, presence: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Tag do
   it { should have_and_belong_to_many :images }
+  it { should validate_presence_of :name }
+  it { should validate_uniqueness_of :name }
 end
